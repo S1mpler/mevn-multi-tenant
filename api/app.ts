a6c2107b7062c683f60a25679ef075c4e2bc2e14
S1mpler <stas.dior@gmail.com>
2020-07-02 08:52:23 +0200

    Add database configuration and global middleware

---
+++ api/app.ts
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+import * as express from 'express';
+import { json, urlencoded } from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as http from 'http';
+
+import { DatabaseConnection } from './config/database';
+
+const app = express();
+
+// Set global middleware
+app.use(json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
+app.use(
+  (
+    err: Error & { status: number },
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ): void => {
+    response.status(err.status || 500);
+    response.json({
+      error: 'Server error',
+    });
+  }
+);
+
+// Connect to database
+new DatabaseConnection().connect({
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbName: process.env.DB_NAME,
+});
+
+// Start the server
+const server: http.Server = app.listen(process.env.PORT || 3000);
+
+export { server };
---
+++ api/config/database.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+
+export class DatabaseConnection {
+  public connect({ dbHost, dbPort, dbName }) {
+    mongoose
+      .connect(`mongodb://${dbHost + ':' + dbPort}/${dbName}`, {
+        useMongoClient: true,
+      })
+      .then(() => {
+        console.log(
+          'Database connection successful to ' + dbHost + ':' + dbPort
+        );
+      })
+      .catch((err) => {
+        console.error('Database connection error');
+        console.error(err);
+      });
+  }
+}
